e7c261b49c30c00ba828e5145024e658510df85e
JhayJ <110433055+JhayJo9@users.noreply.github.com>
2023-06-04 09:01:00 +0800

    Add files via upload

---
+++ MyApp/Modify_init_.py
@@ -0,0 +1,3 @@
+
+from mypackage  import average, power
+from mypackage  import  SayHello
---
+++ MyApp/__init__.py
@@ -0,0 +1,6 @@
+from mypackage .functions import average, power
+from mypackage .greet import SayHello
+
+average(x, y)
+power(x, y)
+SayHello(name)
